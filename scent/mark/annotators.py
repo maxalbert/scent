a47d274954f536e3a909bc6497ead201a018703c
Maximilian Albert <maximilian.albert@flowminder.org>
2016-12-12 23:39:46 +0000

    Problem: Missing annotator for procedural polymorphism.
    Solution: Add one. Closes #5.

--- scent/mark/annotators.py
+++ scent/mark/annotators.py
@@ -4,7 +4,7 @@ Annotators for common code smells.
 
 from functools import wraps
 
-__all__ = ['dup', 'duplication', 'inconsistency', 'hardcoded_value']
+__all__ = ['dup', 'duplication', 'inconsistency', 'hardcoded_value', 'procedural_polymorphism']
 
 
 class MissingLabelError(Exception):
@@ -94,6 +94,34 @@ class hardcoded_value:
         for example as `@scent.mark.hardcoded_value(...)`. We return
         the decorated function unaltered because the only purpose
         of this class is to document that there is duplication.
+        """
+        return func
+
+@duplication(label='procedural-polymorphism-annotator', comment='This is almost an exact copy of the "duplication" annotator.')
+class procedural_polymorphism:
+    """
+    Annotator to mark hard-coded values which should be turned into variables.
+    """
+
+    def __init__(self, label, comment=None):
+        """
+        Args:
+            label (str):  Label to identify all occurrences of this code smell.
+            comment (str, optional):  Optional comment (intended for other human developers).
+
+        """
+        if label == "":
+            raise MissingLabelError("Label must not be empty.")
+
+        # For now we are ignoring all arguments because the only
+        # purpose of this class is to act as an annotation.
+
+    def __call__(self, func):
+        """
+        This is called when the annotator is used as a decorator,
+        for example as `@scent.mark.procedural_polymorphism(...)`.
+        We return the decorated function unaltered because the only
+        purpose of this class is to document that there is a code smell.
         """
         return func
 
